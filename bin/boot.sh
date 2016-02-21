015052701d66fca489eed025b02b6d35c94ce62d
Lawrence, Andrew <andrew.lawrence@kroger.com>
2016-02-21 09:23:09 -0500

    updating the config path

--- bin/boot.sh
+++ bin/boot.sh
@@ -10,4 +10,4 @@ touch $BASE_DIR/logs/nginx/access.log $BASE_DIR/logs/nginx/error.log
 
 (tail -f -n 0 $BASE_DIR/logs/nginx/*.log &)
 
-exec $BASE_DIR/vendor/nginx/sbin/nginx -p $BASE_DIR -c $BASE_DIR/config/nginx.conf
+exec $BASE_DIR/vendor/nginx/sbin/nginx -p $BASE_DIR -c config/nginx.conf
